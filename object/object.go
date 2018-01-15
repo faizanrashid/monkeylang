60b7646f6d8fc784f3cc5cba384a5d67e5df8030
Faizan Rashid <faizanrashid91@gmail.com>
2018-01-15 12:51:24 -0500

    Finished monkey interpreter.

--- object/object.go
+++ object/object.go
@@ -1,6 +1,12 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/faizanrashid/monkey/ast"
+)
 
 type ObjectType string
 
@@ -14,7 +20,77 @@ const (
 	BooleanObj = "BOOLEAN"
 	NullObj    = "NULL"
 	ReturnValueObj = "RETURN_VALUE"
+	ErrorObj = "ERROR"
+	FunctionObj = "FUNCTION"
 )
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s, outer: nil}
+}
+
+type Environment struct {
+	store map[string]Object
+	outer *Environment
+}
+
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
+	return obj, ok
+}
+
+func (e *Environment) Set(name string, val Object) Object {
+	e.store[name] = val
+	return val
+}
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { 
+	return ErrorObj 
+}
+
+func (e *Error) Inspect() string {
+	return "ERROR: " + e.Message
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+func (f *Function) Type() ObjectType { 
+	return FunctionObj
+}
+
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(fn)")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
 
 type ReturnValue struct {
 	Value Object
